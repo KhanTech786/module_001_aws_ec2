5e9079d36aac2ec2ec92b24dcf2159b19e0a47e9
Khan.A <aiymankhan786@gmail.com>
2022-10-05 00:01:00 -0500

    fix instance type error

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "khans_ec2" {
   ami             = var.instance_ami
-  instance_type   = var.instance_tags
+  instance_type   = var.instance_type
   tags            = var.instance_tags
   security_groups = var.instance_sec_grp
   key_name        = var.instance_key
